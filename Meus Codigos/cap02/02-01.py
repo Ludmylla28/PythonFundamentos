3f006625f12d73015a05708ee56a8358dc775fbc
Tiago Pretel <ludsilva@apoioprodesp.sp.gov.br>
2019-01-15 14:31:25 -0200

    meus codigos

---
+++ Meus Codigos/cap02/02-01.py
@@ -0,0 +1,23 @@
+#Soma 
+print(4 + 4)
+
+#Subtração
+print(22 - 43)
+
+#Multiplicação
+print(6 * 9)
+
+#Divisão
+print(3 / 2)
+
+#Potência
+print(4 ** 2)
+
+#Módulo
+print(10 % 3)
+
+#Tipo da variavel
+print(type(5))
+print(type(5.3))
+print(type("amor é uma faca de dois gumes"))
+
