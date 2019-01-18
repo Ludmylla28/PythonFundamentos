4c7c05056098f9f64c8cda8189d64f256abe3865
Tiago Pretel <ludsilva@apoioprodesp.sp.gov.br>
2019-01-18 17:09:03 -0200

    modulo 5

--- Meus Codigos/cap02/cap2-04.py
+++ Meus Codigos/cap02/cap2-04.py
@@ -37,3 +37,97 @@ print(a)
 
 b = a[2]
 print(b)
+
+#operações com listas 
+
+print(listaRemedio)
+
+##atribui a variavel qualquer item de qualquer lista 
+
+l = listaRemedio[1][1]
+print(l) 
+
+f = l * listaRemedio[2][1]
+print(f)
+
+
+#Concatenando listas 
+
+lista_1 = ["Pretel", "Carolina", "Ludmylla"]
+lista_2 = ["Dulcineia","Emerson", "Luan", "Luana"]
+
+print(lista_1 + lista_2)
+
+#operador in 
+## verifica se o numero ou item esta na lista 
+
+print(10 in listaRemedio[2])
+print(100 in listaRemedio[2])
+
+print("Ludmylla" in lista_1)
+
+
+#função built-in 
+
+## a função len retorna o tamanho da lista
+print(len(listaRemedio[0]))
+
+## a função max retorna o maior valor da lista
+print(max(listaRemedio[0]))
+
+## a função min retorna o menor valor da lista
+print(min(listaRemedio[0]))
+
+## a função adiciona um item na lista 
+lista_2.append("Lady")
+
+print(lista_2)
+
+
+## Criando uma lista vazia 
+peixe = []
+
+print(peixe)
+
+# adicionando um item na lista vazia 
+peixe.append("salmão")
+peixe.append("sardinha")
+peixe.append("atum")
+
+print(peixe)
+
+#criando outra lista vazia 
+pratos = []
+print(pratos)
+
+##copiando os itens de uma lista pra outra 
+for item in peixe:
+    pratos.append(item)
+
+print(pratos)
+
+# conta quantos itens iguais aquele tem 
+print(listaRemedio[0].count(1))
+
+# adicionando varios itens na lista
+pratos.extend(["lasanha", "pizza", "macarronada"])
+print(pratos)
+
+print(pratos.index("lasanha"))
+
+# adicionando um iten na posição desejada da lista
+pratos.insert(2,3)
+print(pratos)
+
+#remove um item da lista 
+pratos.remove(3)
+print(pratos)
+
+#embaralha os itens da lista
+pratos.sort()
+print(pratos)
+
+#reverte os itens da lista
+pratos.reverse()
+print(pratos)
+
---
+++ Meus Codigos/cap02/cap2-05.py
@@ -0,0 +1,17 @@
+#isso é uma lista 
+familia = ["luan", "luana", "lady", "dulci", "emerson"]
+print(familia)
+
+#isso é um dicionario
+familiaIdade = {"Luan": 12, "Luana": 12, "lady": 5, "Dulcy": 43, "Emerson": 45 }
+print(familiaIdade)
+
+print(familiaIdade["lady"])
+
+#limpar dicionario
+familiaIdade.clear()
+print(familiaIdade)
+
+#deletar o dicionario 
+del familiaIdade
+
